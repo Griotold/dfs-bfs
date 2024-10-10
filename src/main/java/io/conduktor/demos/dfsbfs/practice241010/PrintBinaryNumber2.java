4fd5071f3bfe8cc0ffd6528a34427eabf5ca4533
Griotold <gotjd9773@naver.com>
2024-10-10 10:03:25 +0900

    2 이진수 출력(재귀함수)

---
+++ src/main/java/io/conduktor/demos/dfsbfs/practice241010/PrintBinaryNumber2.java
@@ -0,0 +1,23 @@
+package io.conduktor.demos.dfsbfs.practice241010;
+
+import java.util.Scanner;
+
+// 2. 이진수 출력(재귀)
+public class PrintBinaryNumber2 {
+
+    public void DFS(int n) {
+        if (n == 1) {
+            System.out.print(1 + " ");
+        } else {
+            DFS(n / 2);
+            int binaryNumber = n % 2;
+            System.out.print(binaryNumber + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintBinaryNumber2 T = new PrintBinaryNumber2();
+        Scanner scanner = new Scanner(System.in);
+        T.DFS(Integer.parseInt(scanner.nextLine()));
+    }
+}
