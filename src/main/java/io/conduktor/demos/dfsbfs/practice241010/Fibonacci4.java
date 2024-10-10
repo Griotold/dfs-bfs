9ecdcc138ba2386743b00cd824b8c9b5ba53c278
Griotold <gotjd9773@naver.com>
2024-10-10 10:27:10 +0900

    4. 피보나치 재귀(메모이제이션)

---
+++ src/main/java/io/conduktor/demos/dfsbfs/practice241010/Fibonacci4.java
@@ -0,0 +1,26 @@
+package io.conduktor.demos.dfsbfs.practice241010;
+// 4. 피보나치 재귀(메모이제이션)
+public class Fibonacci4 {
+
+    static int[] fibo;
+
+    int DFS(int n) {
+        if (fibo[n] > 0) return fibo[n];
+        if (n == 1 || n == 2) {
+            return fibo[n] = 1;
+        } else {
+            return fibo[n] = DFS(n - 2) + DFS(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Fibonacci4 fib = new Fibonacci4();
+        int n = 13;
+        fibo = new int[n+1];
+        fib.DFS(n);
+        for (int i : fibo) {
+            if (i == 0) continue;
+            System.out.print(i + " ");
+        }
+    }
+}
