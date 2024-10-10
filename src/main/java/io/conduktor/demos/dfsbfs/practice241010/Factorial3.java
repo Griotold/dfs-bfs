eeedbc0a56020a6e8a97e503533487d4e81f6134
Griotold <gotjd9773@naver.com>
2024-10-10 10:09:00 +0900

    3. 팩토리얼

---
+++ src/main/java/io/conduktor/demos/dfsbfs/practice241010/Factorial3.java
@@ -0,0 +1,17 @@
+package io.conduktor.demos.dfsbfs.practice241010;
+// 3. 팩토리얼
+public class Factorial3 {
+
+    int DFS(int n) {
+        if (n == 1) return 1;
+        else {
+            return n * DFS(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Factorial3 T = new Factorial3();
+        int value = T.DFS(5);
+        System.out.println(value);
+    }
+}
