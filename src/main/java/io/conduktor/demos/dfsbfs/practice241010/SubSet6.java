72cb31bda3e27b9e86e4382b2307928292d90fe9
Griotold <gotjd9773@naver.com>
2024-10-11 10:11:13 +0900

    6. 부분집합 구하기(DFS)

---
+++ src/main/java/io/conduktor/demos/dfsbfs/practice241010/SubSet6.java
@@ -0,0 +1,27 @@
+package io.conduktor.demos.dfsbfs.practice241010;
+// 부분집합 구하기(DFS)
+public class SubSet6 {
+    static int[] ch;
+    static int N;
+    void dfs(int L) {
+        if (L == N + 1) {
+            for (int i = 1; i <= N; i++) {
+                if (ch[i] != 0) System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        else {
+            ch[L] = 1;
+            dfs(L + 1);
+            ch[L] = 0;
+            dfs(L + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        SubSet6 T = new SubSet6();
+        N = 3;
+        ch = new int[N + 1];
+        T.dfs(1);
+    }
+}
