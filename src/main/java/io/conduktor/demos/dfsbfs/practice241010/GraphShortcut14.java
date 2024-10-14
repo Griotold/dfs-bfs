78c91bb24888e7a1a7bcd8798a83b2625e6763f7
Griotold <gotjd9773@naver.com>
2024-10-14 10:48:43 +0900

    14. 그래프 최단거리(BFS) - 나는 인접행렬로, 답지는 인접리스트로

---
+++ src/main/java/io/conduktor/demos/dfsbfs/practice241010/GraphShortcut14.java
@@ -0,0 +1,60 @@
+package io.conduktor.demos.dfsbfs.practice241010;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+// 14. 그래프 최단거리(BFS)
+public class GraphShortcut14 {
+
+    static int N;
+    static int M;
+    static int[][] graph;
+    static int[] ch;
+    static int[] answer;
+    static Deque<Integer> queue;
+
+    int bfs(int s, int e) {
+        ch[s] = 1;
+        queue.offer(s);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int length = queue.size();
+            for (int i = 0; i < length; i++) {
+                int x = queue.poll();
+                if (x == e) return level;
+                for (int j = 1; j <= N; j++) {
+                    if (ch[j] == 0 && graph[x][j] == 1) {
+                        ch[j] = 1;
+                        queue.offer(j);
+                    }
+                }
+            }
+            level++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        GraphShortcut14 gs = new GraphShortcut14();
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        graph = new int[N + 1][N + 1];
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a][b] = 1;
+        }
+        ch = new int[N + 1];
+        answer = new int[N + 1];
+        for (int i = 2; i <= N; i++) {
+            queue = new ArrayDeque<>();
+            ch = new int[N + 1];
+            answer[i] = gs.bfs(1, i);
+        }
+        for (int i = 2; i <= N; i++) {
+            System.out.println(i + " : " + answer[i]);
+        }
+    }
+}
---
+++ src/main/java/io/conduktor/demos/dfsbfs/practice241010/GraphShortcut14_2.java
@@ -0,0 +1,55 @@
+package io.conduktor.demos.dfsbfs.practice241010;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Scanner;
+
+// 14. 그래프 최단거리(BFS) - 인접리스트
+public class GraphShortcut14_2 {
+
+    static int N;
+    static int M;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch;
+    static int[] dis;
+
+    void bfs(int s) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        ch[s] = 1;
+        dis[s] = 0;
+        queue.offer(s);
+        while (!queue.isEmpty()) {
+            int currentNode = queue.poll();
+            for (int nextNode : graph.get(currentNode)) {
+                if (ch[nextNode] == 0) {
+                    ch[nextNode] = 1;
+                    queue.offer(nextNode);
+                    dis[nextNode] = dis[currentNode] + 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphShortcut14_2 T = new GraphShortcut14_2();
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        ch = new int[N + 1];
+        dis = new int[N + 1];
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
+        T.bfs(1);
+        for (int i = 2; i <= N; i++) {
+            System.out.println(i + " : " + dis[i]);
+        }
+    }
+}
